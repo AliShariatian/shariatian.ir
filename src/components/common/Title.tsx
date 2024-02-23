8d1643d6dd17a3ec95670a860117fe5adb5ed501
Ali Shariatian <shariatian92@gmail.com>
2024-02-23 21:32:42 +0330

    remove unused components

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -5,10 +5,10 @@ import { motion } from "framer-motion";
 
 type Props = {
    title: string;
-   classNames: string;
+   classNames?: string;
 };
 
-const Title: FC<Props> = ({ title = "عنوان", classNames = "" }): JSX.Element => {
+const Title: FC<Props> = ({ title, classNames }): JSX.Element => {
    return (
       <div className={`relative text-ellipsis text-center ${classNames}`}>
          <span className="absolute right-0 -top-4 xl:-top-9 -z-10 w-full text-slate-900 opacity-80 select-none font-morabba text-4xl xl:text-8xl font-bold">{title}</span>
