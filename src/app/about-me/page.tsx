d50c19550c7814b16f6355d2a35ed5819f574761
Ali Shariatian <shariatian92@gmail.com>
2024-02-24 20:09:38 +0330

    add effect when in view

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -5,6 +5,7 @@ import Title from "@/components/common/Title";
 import { aboutMePageTexts, metaTitle } from "@/utils/persianTexts";
 import ProjectCard from "@/components/ProjectCart";
 import Image from "next/image";
+import FadeInWhenVisible from "@/components/common/FadeInWhenVisible";
 
 export const metadata: Metadata = {
    title: `${metaTitle} درباره من`,
@@ -21,27 +22,30 @@ const AboutMePage: FC = (): JSX.Element => {
          <p className="px-16 xl:px-5 xl:w-1/2 text-slate-300 leading-8 text-justify">{aboutMePageTexts.aboutMe}</p>
          {/* Arrow */}
          <Image src="/img/curved-arrow.svg" alt="arrow" width={90} height={90} className="mt-9 invert opacity-70 scale-x-[-1] -rotate-[15deg]" />
-
          <section id="projects" className="mt-20 flex flex-col items-center justify-center">
-            <h4 className="font-extrabold text-4xl my-36">{aboutMePageTexts.projects.projectsTitle}</h4>
+            <FadeInWhenVisible>
+               <h4 className="font-extrabold text-4xl mt-48 mb-28">{aboutMePageTexts.projects.projectsTitle}</h4>
+            </FadeInWhenVisible>
 
             {/* Projects card */}
-            <div className="grid grid-cols-1 xl:grid-cols-3 gap-10 px-5 xl:px-16">
-               <ProjectCard
-                  link="galaxy.shariatian.ir"
-                  src="/img/projects/galaxy.webp"
-                  title="لندینگ پیج گلکسی"
-                  description="پروژه تک‌صفحه‌ای لندینگ پیج برای نمایش نمونه‌کارها"
-                  skills={["React JS", "Next JS", "Tailwind CSS", "Framer Motion"]}
-               />
-               <ProjectCard
-                  link="medium.shariatian.ir"
-                  src="/img/projects/medium.webp"
-                  title="کلون سایت Medium"
-                  description="پروژه مشابه‌سازی سایت وبلاگ‌نویسی Medium"
-                  skills={["React JS", "Context API", "Axios", "EditorJS", "Lodash", "Tailwind CSS", "Framer Motion"]}
-               />
-            </div>
+            <FadeInWhenVisible>
+               <div className="grid grid-cols-1 xl:grid-cols-3 gap-10 px-5 xl:px-20">
+                  <ProjectCard
+                     link="galaxy.shariatian.ir"
+                     src="/img/projects/galaxy.webp"
+                     title="لندینگ پیج گلکسی"
+                     description="پروژه تک‌صفحه‌ای لندینگ پیج برای نمایش نمونه‌کارها"
+                     skills={["React JS", "Next JS", "Tailwind CSS", "Framer Motion"]}
+                  />
+                  <ProjectCard
+                     link="medium.shariatian.ir"
+                     src="/img/projects/medium.webp"
+                     title="کلون سایت Medium"
+                     description="پروژه مشابه‌سازی سایت وبلاگ‌نویسی Medium"
+                     skills={["React JS", "Context API", "Axios", "EditorJS", "Lodash", "Tailwind CSS", "Framer Motion"]}
+                  />
+               </div>
+            </FadeInWhenVisible>
          </section>
       </div>
    );
---
+++ src/components/common/FadeInWhenVisible.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { FC, PropsWithChildren } from "react";
+import { motion } from "framer-motion";
+
+const FadeInWhenVisible: FC<PropsWithChildren> = ({ children }) => {
+   return (
+      <motion.div
+         initial="hidden"
+         whileInView="visible"
+         viewport={{ once: true }}
+         transition={{ duration: 0.5 }}
+         variants={{
+            visible: { opacity: 1, scale: 1 },
+            hidden: { opacity: 0, scale: 0 },
+         }}
+      >
+         {children}
+      </motion.div>
+   );
+};
+
+export default FadeInWhenVisible;
