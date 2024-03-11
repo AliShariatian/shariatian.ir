9f8267551b591d1cfd2b4a4a036698b60ef3e156
Ali Shariatian <shariatian92@gmail.com>
2024-03-12 01:30:02 +0330

    💄 style: add box shadow in hover for project card

--- src/components/sub/ProjectCart.tsx
+++ src/components/sub/ProjectCart.tsx
@@ -25,7 +25,7 @@ const ProjectCard: FC<Props> = ({
    imgBgColor = "bg-blue-200",
 }): JSX.Element => {
    return (
-      <div className="group/img relative overflow-hidden rounded-2xl border border-slate-900 bg-slate-950 shadow-md">
+      <div className="group/img relative overflow-hidden rounded-2xl border border-slate-900 bg-slate-950 shadow-2xl transition ease-out hover:border-slate-500/40 hover:shadow-slate-500/20">
          {/* Image */}
          <A href={liveLink} target="_blank" className="relative">
             <div className={`${imgBgColor} overflow-hidden rounded-2xl p-9`}>
